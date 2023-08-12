19ceda0f8aad26cdc4c80f459164e765cf0f1288
“pam2samy” <pambosam@gmail.com>
2023-08-11 17:55:28 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int s = 0, e = 8, s1 = 1, e2 = 9;
+
+	while (s <= e)
+	{
+		s1 = s + 1;
+
+		while (s1 <= e2)
+		{
+			putchar(s + '0');
+			putchar(s1 + '0');
+			s1++;
+
+			if (s != e)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		s++;
+	}
+	putchar('\n');
+	return (0);
+}
+
