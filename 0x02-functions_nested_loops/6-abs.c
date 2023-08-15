903ca1484c26a8f410379b509232e69e11f04969
“pam2samy” <pambosam@gmail.com>
2023-08-15 13:17:35 -0700

    print_last_digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,10 +2,9 @@
 
 /**
  * _asb - Computes the absolute value of an integer.
- * @c: The number to be computed
+ * @c: The number to be computed.
  * Return: Absolute value of number or zero
  */
-
 int _abs(int c)
 
 {
