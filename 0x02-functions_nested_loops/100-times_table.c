cac96b3a367bec32ba362f876e72f67724e13078
“pam2samy” <pambosam@gmail.com>
2023-08-15 15:42:20 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print n times table
+ * @n: Parameter
+ * Return: Empty output
+ */
+void print_times_table(int n)
+{
+	int p, q, r;
+
+	if (n <= 15 && n >= 0)
+		for (p = 0; p <= n; p++)
+		{
+			_putchar('0');
+			for (q = 1; q <= n; q++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				r = p * q;
+				if (r <= 98)
+					_putchar(' ');
+				if (r <= 9)
+					_putchar(' ');
+				if (r >= 100)
+				{
+					_putchar((r / 100) + '0');
+					_putchar(((r / 10) % 10) + '0');
+				}
+				else if (r <= 99 && r >= 10)
+				{
+					_putchar((r / 10) + '0');
+				}
+				_putchar((r % 10) + '0');
+			}
+			_putchar('\n');
+		}
+}
