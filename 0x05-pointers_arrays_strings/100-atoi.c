a5b424264cacd8c126ab46b75a8833764a54ca16
“pam2samy” <pambosam@gmail.com>
2023-08-21 13:59:16 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,15 +9,28 @@
 int _atoi(char *s)
 {
 	int c = 1
-	unsigned int num = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	do {
-		if (*s == '-')
-			c *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '9');
-		else if (num > 0)
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
 			break;
-	} while (*s++);
-	return (num * c);
+		}
+		c++;
+	}
+	ni *= min;
+	return (ni);
 }
