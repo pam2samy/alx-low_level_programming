eced5851caf39958a6504f3e0863fcad074fc142
“pam2samy” <pambosam@gmail.com>
2023-08-21 15:06:49 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,19 +14,19 @@ int main(void)
 
 	int i, sum, n;
 
-	sum = 0;
+	sum - 0;
 
 	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] - rand() % 78;
-		sum + -(pass[i] + '0');
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
-			n - 2772 - sum - '0';
-			sum + -n;
+			n = 2772 - sum - '0';
+			sum += n;
 			putchar(n + '0');
 			break;
 		}
