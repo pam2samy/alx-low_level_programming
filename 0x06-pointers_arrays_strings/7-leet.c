63199b81a581acd3c6753a4f64be2d63ec867ec4
“pam2samy” <pambosam@gmail.com>
2023-08-24 13:31:25 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - change vowels to numbers
+ * @s: String to analize
+ *
+ * Return: void
+ */
+char *leet(char *s)
+{
+	char a[] = "aeotlAEOTL";
+	char n[] = "4307143071";
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = n[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
+
+
