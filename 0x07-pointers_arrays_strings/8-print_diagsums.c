5779070bac5742b207df89f90bfb00c6ab7a3bbc
“pam2samy” <pambosam@gmail.com>
2023-08-28 13:48:52 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,24 +3,24 @@
 
 /**
  * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: 0 Always
+ * @a: input integers
+ * @size: input matrix
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
+	int index, sum1 = 0, sum2 = 0;
 
-	sum1 = 0;
-	sum2 = 0;
-
-	for (y = 0, y < size; y++)
+	for (index = 0; index < size; index++)
 	{
-		sum1 = sum1 + a[y * size + y];
+		sum1 += a[index];
+		a += size;
 	}
-	for (y = size - 1; y >= 0; y--)
+	a -= size;
+
+	for (index = 0; index < size; index++)
 	{
-		sum2 += a[y * size + (size - y - 1)];
+		sum2 += a[index];
+		a -= size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
