3b6de1ea9e1e28fee8f7411986c811e181a26f01
“pam2samy” <pambosam@gmail.com>
2023-08-30 10:00:09 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,13 +21,13 @@ int _sqrt_recursion(int n)
  * @n: number  to calculate sqrt of
  * @i: iterator
  *
- * Return: the resulting square root of
+ * Return: the resulting square root
  */
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-		return (1);
+		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
 }
