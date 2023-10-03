8c54d8f3167d5bfc51c08a8e247242b6de4f09f4
pam2samy <pambosam@gmail.com>
2023-10-03 14:02:40 -0700

    File IN/OUT

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,11 @@
 #include <elf.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_osabi_more(Elf64_Ehdr h);
 
