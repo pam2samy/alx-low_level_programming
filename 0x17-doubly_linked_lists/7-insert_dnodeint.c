d611061d1d9196d4df77206b2b50ab6a5e13924d
“pam2samy” <pambosam@gmail.com>
2023-12-08 09:05:49 -0800

    D-linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,38 +4,53 @@
  * insert_dnodeint_at_index - inserts a new node
  * @h: A pointer to the head
  * @idx: insert position
- * @n: the integer
+ * @n: integer value of the node
  *
- * Return: Address node, or NULL.
+ * Return: Address node, or NULL,
  *              if the func fails
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-        dlistint_t *tmp = *h, *new;
+        dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-        if (idx == 0)
-                return (add_dnodeint(h, n));
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	while
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+				new = add_dnodeint_end(h, n);
+				
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
-        for (; idx != 1; idx--)
-        {
-                tmp = tmp->next;
-                if (tmp == NULL)
-                        return (NULL);
-        }
-
-        if (tmp->next == NULL)
-                return (add_dnodeint_end(h, n));
-
-        new = malloc(sizeof(dlistint_t));
-        if (new == NULL)
-                return (NULL);
-
-        new->n = n;
-        new->prev = tmp;
-        new->next = tmp->next;
-        tmp->next->prev = new;
-        tmp->next = new;
-
-        return (new);
+	return (new);
 }
